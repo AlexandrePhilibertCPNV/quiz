5f73f0f37e36293fa304f294149dabfc00aa9387
AlexandrePhilibert <philibert.alexandre@gmail.com>
2022-03-07 14:10:52 +0100

    Add Question model

---
+++ lib/models/question.dart
@@ -0,0 +1,19 @@
+class Question {
+  String caption;
+  List<String> answers;
+  String correctAnswer;
+  String hint;
+
+  Question(this.caption, this.answers, this.correctAnswer, this.hint);
+
+  factory Question.fromJson(Map<String, dynamic> json) {
+    List<String> answers = json['answers'].cast<String>();
+
+    return Question(
+      json['caption'],
+      answers,
+      answers[json['correct_answer_index']],
+      json['hint'],
+    );
+  }
+}
