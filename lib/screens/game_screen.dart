ebbf59a5414088777233d01a3de07bced847fb33
AlexandrePhilibert <philibert.alexandre@gmail.com>
2022-03-08 08:20:41 +0100

    Improve game screen layout

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -19,18 +19,50 @@ class GameScreen extends StatelessWidget with GetItMixin {
       ),
       body: Center(
         child: questionSnapshot.hasData
-            ? Column(
-                children: [
-                  Text(question.caption),
-                  Row(
-                    children: question.answers.map((answer) {
-                      return ElevatedButton(
-                        child: Text(answer),
-                        onPressed: () {},
-                      );
-                    }).toList(),
-                  )
-                ],
+            ? Container(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 20.0,
+                  vertical: 28.0,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      question.caption,
+                      style: const TextStyle(fontSize: 24.0),
+                    ),
+                    const Spacer(
+                      flex: 1,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: question.answers.map(
+                        (answer) {
+                          return Container(
+                            margin: const EdgeInsets.symmetric(
+                              horizontal: 10.0,
+                            ),
+                            child: TextButton(
+                              style: TextButton.styleFrom(
+                                backgroundColor: Colors.transparent,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(6.0),
+                                  side: const BorderSide(
+                                    color: Colors.blue,
+                                    width: 2,
+                                    style: BorderStyle.solid,
+                                  ),
+                                ),
+                              ),
+                              child: Text(answer),
+                              onPressed: () {},
+                            ),
+                          );
+                        },
+                      ).toList(),
+                    ),
+                  ],
+                ),
               )
             : const CircularProgressIndicator(),
       ),
